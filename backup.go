d2415bec9c9b9949746a06fd7855a86f8c6b48a3
Sven Dowideit <SvenDowideit@home.org.au>
2020-09-04 13:00:50 +1000

    output in json (or whatever)
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- backup.go
+++ backup.go
@@ -3,10 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
+	"log"
 
 	"github.com/libdns/digitalocean"
-	"github.com/libdns/libdns"
+	"github.com/onaci/docker-ona/outputs"
 )
 
 type BackupCmd struct {
@@ -15,6 +15,8 @@ type BackupCmd struct {
 
 	//DryRun bool `description:"Don't actually download or update, just show what it would change"`
 	DoToken string `help:"Digital Ocean API key" env:"DO_API_KEY"`
+
+	Format string `default:"json" help:"output format (json, spew, printf)"`
 }
 
 func (cmd *BackupCmd) Run(ctx *Context) error {
@@ -25,47 +27,55 @@ func (cmd *BackupCmd) Run(ctx *Context) error {
 		fmt.Printf("ERROR: %s\n", err.Error())
 	}
 
-	testName := "libdns-test"
-	testId := ""
-	for _, record := range records {
-		fmt.Printf("%s (.%s): %s, %s, %d\n", record.Name, cmd.Zone, record.Value, record.Type, record.TTL)
-		if record.Name == testName {
-			testId = record.ID
-		}
+	// testName := "libdns-test"
+	// testId := ""
+	// for _, record := range records {
+	// 	fmt.Printf("%s (.%s): %s, %s, %d\n", record.Name, cmd.Zone, record.Value, record.Type, record.TTL)
+	// 	if record.Name == testName {
+	// 		testId = record.ID
+	// 	}
 
+	// }
+	res := make([]interface{}, len(records))
+	for i, v := range records {
+		res[i] = v
+	}
+	err = outputs.FormatArray(log.Writer(), res, "", cmd.Format)
+	if err != nil {
+		return err
 	}
 
-	if testId != "" {
-		// fmt.Printf("Delete entry for %s (id:%s)\n", testName, testId)
-		// _, err = provider.DeleteRecords(context.TODO(), zone, []libdns.Record{libdns.Record{
-		// 	ID: testId,
-		// }})
-		// if err != nil {
-		// 	fmt.Printf("ERROR: %s\n", err.Error())
-		// }
-		// Set only works if we have a record.ID
-		fmt.Printf("Replacing entry for %s\n", testName)
-		_, err = provider.SetRecords(context.TODO(), cmd.Zone, []libdns.Record{libdns.Record{
-			Type:  "TXT",
-			Name:  testName,
-			Value: fmt.Sprintf("Replacement test entry created by libdns %s", time.Now()),
-			TTL:   time.Duration(30) * time.Second,
-			ID:    testId,
-		}})
-		if err != nil {
-			fmt.Printf("ERROR: %s\n", err.Error())
-		}
-	} else {
-		fmt.Printf("Creating new entry for %s\n", testName)
-		_, err = provider.AppendRecords(context.TODO(), cmd.Zone, []libdns.Record{libdns.Record{
-			Type:  "TXT",
-			Name:  testName,
-			Value: fmt.Sprintf("This is a test entry created by libdns %s", time.Now()),
-			TTL:   time.Duration(30) * time.Second,
-		}})
-		if err != nil {
-			fmt.Printf("ERROR: %s\n", err.Error())
-		}
-	}
+	// if testId != "" {
+	// 	// fmt.Printf("Delete entry for %s (id:%s)\n", testName, testId)
+	// 	// _, err = provider.DeleteRecords(context.TODO(), zone, []libdns.Record{libdns.Record{
+	// 	// 	ID: testId,
+	// 	// }})
+	// 	// if err != nil {
+	// 	// 	fmt.Printf("ERROR: %s\n", err.Error())
+	// 	// }
+	// 	// Set only works if we have a record.ID
+	// 	fmt.Printf("Replacing entry for %s\n", testName)
+	// 	_, err = provider.SetRecords(context.TODO(), cmd.Zone, []libdns.Record{libdns.Record{
+	// 		Type:  "TXT",
+	// 		Name:  testName,
+	// 		Value: fmt.Sprintf("Replacement test entry created by libdns %s", time.Now()),
+	// 		TTL:   time.Duration(30) * time.Second,
+	// 		ID:    testId,
+	// 	}})
+	// 	if err != nil {
+	// 		fmt.Printf("ERROR: %s\n", err.Error())
+	// 	}
+	// } else {
+	// 	fmt.Printf("Creating new entry for %s\n", testName)
+	// 	_, err = provider.AppendRecords(context.TODO(), cmd.Zone, []libdns.Record{libdns.Record{
+	// 		Type:  "TXT",
+	// 		Name:  testName,
+	// 		Value: fmt.Sprintf("This is a test entry created by libdns %s", time.Now()),
+	// 		TTL:   time.Duration(30) * time.Second,
+	// 	}})
+	// 	if err != nil {
+	// 		fmt.Printf("ERROR: %s\n", err.Error())
+	// 	}
+	// }
 	return nil
 }
